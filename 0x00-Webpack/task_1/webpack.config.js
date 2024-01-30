00aaf809a02d606dc91f079d8cdd99ce0e9e5158
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-30 09:57:40 +0100

    task 1 and task 2

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: {
-		main: path.resolve(__dirname, './js/dashboard_main.js'),
-	},
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
 };
