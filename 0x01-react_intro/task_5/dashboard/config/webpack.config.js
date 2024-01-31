f7c6eedc9616cf675739a0b141c68dc60ac8fd6f
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-01 00:10:55 +0100

    fixing webpack config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
 	},
 	mode: 'development',
 	module: {
