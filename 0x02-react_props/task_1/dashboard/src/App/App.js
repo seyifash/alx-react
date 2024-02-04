30fd75fcf3198ba83597d9b7cf41b6ac3c3ccdc8
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-04 22:14:46 +0100

    task-1

---
+++ 0x02-react_props/task_1/dashboard/src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+
+function App() {
+		return (
+		<React.Fragment>
+			<Notifications />
+			<div className="App">
+				<Header />
+				<Login />
+				<Footer />
+			</div>
+		</React.Fragment>
+		);
+}
+
+export default App;
