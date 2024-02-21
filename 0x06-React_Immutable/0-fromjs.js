e55382ef6a80dadba1ace24572b5a6309ebbe2da
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-21 15:19:17 +0000

    requirement satusfied

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,8 @@
 import { fromJS } from 'immutable';
 
 function getImmutableObject(object) {
-    const immutableMap = fromJS(object);
-    return immutableMap;
+  const immutableMap = fromJS(object);
+  return immutableMap;
 }
 
 export default getImmutableObject;
-
